b48989b8648e203b4c8b6e3cdd9d022fc35421a9
Jack Harvey <jack.harvey@news.co.uk>
2018-09-09 17:39:59 +0100

    Add 6 image assets

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as breakfast } from './breakfast.png';
+export { default as getReady } from './get-ready.png';
+export { default as shoes } from './shoes.png';
+export { default as timedVideoGame } from './timed-video-game.png';
+export { default as toysTidiedUp } from './toys-tidied-up.png';
+export { default as washYourHands } from './breakfast.png';
