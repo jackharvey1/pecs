a9d7ab71fb2806b2c1a810b9e30dd2b6455433dc
Jack Harvey <jack.harvey@news.co.uk>
2018-09-13 19:31:08 +0100

    Fix path for wash-your-hands.png

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,4 @@ export { default as getReady } from './get-ready.png';
 export { default as shoes } from './shoes.png';
 export { default as timedVideoGame } from './timed-video-game.png';
 export { default as toysTidiedUp } from './toys-tidied-up.png';
-export { default as washYourHands } from './breakfast.png';
+export { default as washYourHands } from './wash-your-hands.png';
