dd6f9b407a8d9ac6f67468808482893495354d49
Jack Harvey <jack.harvey@news.co.uk>
2018-09-14 23:19:35 +0100

    Output classes to snapshots with jest-styled-components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from '../components/App';
 
 it('renders renders as intended', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = renderer.create(<App />).toJSON();
     expect(wrapper).toMatchSnapshot();
 });
