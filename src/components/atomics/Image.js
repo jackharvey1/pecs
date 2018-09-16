ab7d17a8b7e52d1b2a9490037f9327ced87e4a6a
Jack Harvey <jack.harvey@news.co.uk>
2018-09-17 00:46:56 +0100

    Enforce single quotes

--- src/components/atomics/Image.js
+++ src/components/atomics/Image.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ShadowedImage = styled.img`
     box-shadow: 0 2px 8px 0 rgba(0, 0, 0, .25);
--- src/stories/composites/PictureBoard.stories.js
+++ src/stories/composites/PictureBoard.stories.js
@@ -7,19 +7,19 @@ storiesOf('PictureBoard')
     .add('empty', () => <PictureBoard />)
     .add('with one image', () => (<PictureBoard data={[
         {
-            text: "Caption",
-            src: "https://via.placeholder.com/300x200",
+            text: 'Caption',
+            src: 'https://via.placeholder.com/300x200',
         },
     ]} />))
     .add('with multiple images', () => (<PictureBoard data={[
         {
-            text: "Caption",
-            src: "https://via.placeholder.com/300x200",
+            text: 'Caption',
+            src: 'https://via.placeholder.com/300x200',
         }, {
-            text: "Caption2",
-            src: "https://via.placeholder.com/300x200",
+            text: 'Caption2',
+            src: 'https://via.placeholder.com/300x200',
         }, {
-            text: "Caption3",
-            src: "https://via.placeholder.com/300x200",
+            text: 'Caption3',
+            src: 'https://via.placeholder.com/300x200',
         },
     ]} />));
