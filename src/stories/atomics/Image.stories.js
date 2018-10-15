e32af60352de751b7c99cf08ad30a9ed1876f9e3
Jack Harvey <jackrrv@gmail.com>
2018-10-15 19:29:41 +0100

    Remove storybook

--- src/stories/atomics/Image.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Image from '../../components/atomics/Image';
-
-storiesOf('Image')
-    .add('with image', () => <Image src="https://via.placeholder.com/300x200" />);
